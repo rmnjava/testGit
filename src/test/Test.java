34f36315afa96a483e616551dfddb8342ad9ab57
rmnjava <snowdaning@163.com>
2017-03-06 14:59:56 +0800

    新增class

---
+++ src/test/Test.java
@@ -0,0 +1,30 @@
+/**
+ * Project Name:test
+ * File Name:Test.java
+ * Package Name:test
+ * Date:2017年3月6日下午2:59:24
+ * Copyright (c) 2017, China Link Communications LTD All Rights Reserved.
+ *
+ */
+
+
+package test;
+
+/**
+ * ClassName: Test <br/>
+ * Date: 2017年3月6日 下午2:59:24 <br/>
+ * Description: TODO 
+ *
+ * @author lenovo
+ * @version 
+ * @see
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1111111");
+
+    }
+
+}
+
